d27c430ecf3251d63edf5545bdd014136679885c
Johnny Alexander Pineda <alexkrimen@gmail.com>
2026-04-12 17:42:16 -0500

    Movil visual

--- app/views/simulador.php
+++ app/views/simulador.php
@@ -42,7 +42,7 @@ $categorias = [
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Simulador de Cotizaciones</title>
 
-  <link rel="icon" href="../assets/img/favicon.ico" type="image/x-icon" />
+  <link rel="icon" href="</JB-CONSTRUCCIONES/assets/img/favicon.ico" type="image/x-icon" />
   <script src="https://cdn.tailwindcss.com"></script>
   <script src="https://cdnjs.cloudflare.com/ajax/libs/html2pdf.js/0.10.1/html2pdf.bundle.min.js"></script>
   <script src="https://cdnjs.cloudflare.com/ajax/libs/xlsx/0.18.5/xlsx.full.min.js"></script>
